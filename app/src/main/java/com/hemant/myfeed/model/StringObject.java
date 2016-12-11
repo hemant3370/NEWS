e1aa66159d35c61e5e8a5e674d0f6944fe5360ae
Hemant Singh <hemant3370@Gmail.com>
2016-12-11 09:33:33 +0530

    Create StringObject.java

---
+++ app/src/main/java/com/hemant/myfeed/model/StringObject.java
@@ -0,0 +1,11 @@
+public class StringObject extends RealmObject {
+  private String val;
+
+  public String getValue() {
+    return val;
+  }
+
+  public void setValue(String value) {
+    this.val = value;
+  }
+}
